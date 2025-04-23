e4870505e59bf127e4500df34c814d5c148f600c
Robbhedonic <143946750+Robbhedonic@users.noreply.github.com>
2025-04-23 20:53:15 +0200

    fix: bugs

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue-projects/',
+  base: process.env.NODE_ENV === 'production' ? '/vue-project/' : '/',
   plugins: [vue(), vuetify({ autoImport: true })],
 })
